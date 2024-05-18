e2aeef725be82258a301fca684242970393de7ad
smorihira <mos.aif.0924@gmail.com>
2024-05-18 18:58:42 +0900

    fix: GenerateRandomString

--- client/src/utils/GenerateRandomString.tsx
+++ client/src/utils/GenerateRandomString.tsx
@@ -1,5 +1,7 @@
-export const generateRandomString = (length: number): string => { // 指定の長さのランダム文字列生成
+const GenerateRandomString = (length: number): string => { // 指定の長さのランダム文字列生成
     const array = new Uint8Array(length);
     window.crypto.getRandomValues(array);
     return Array.from(array, byte => ('0' + byte.toString(16)).slice(-2)).join(''); // 16進数に変換し文字列に変換
 };
+
+export default GenerateRandomString;
