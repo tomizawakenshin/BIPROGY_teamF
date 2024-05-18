dbef305d63128429e09d0963d4ce0a8dede7a20e
smorihira <mos.aif.0924@gmail.com>
2024-05-19 01:29:44 +0900

    ログイン処理と同時にユーザ情報の登録も行う

--- client/src/components/LoginCallback.tsx
+++ client/src/components/LoginCallback.tsx
@@ -26,11 +26,31 @@ const Callback = () => {
                 });
 
                 // c.f. レスポンス形式：https://developers.line.biz/ja/docs/line-login/integrate-line-login/#response
-                sessionStorage.setItem('accessToken', response.data.access_token);
+                const accessToken = response.data.access_token;
+                const idToken = response.data.id_token;
+
+                sessionStorage.setItem('accessToken', accessToken);
+
+                // ユーザ情報を取得：https://developers.line.biz/ja/reference/line-login/#get-user-profile
+                const userInfoResponse = await axios.get('https://api.line.me/v2/profile', {
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`
+                    }
+                });
+
+                const userInfo = userInfoResponse.data;
+
+                // バックエンドにユーザ情報を送信してデータベースに登録及びログイン処理(クッキーの設定)
+                await axios.post('http://localhost:3000/api/register', { // TODO: ユーザ作成のエンドポイントはまだ適当
+                    userId: userInfo.userId,
+                    displayName: userInfo.displayName,
+                    idToken: idToken
+                });
             } catch (error) {
-                const errUrlPrams = new URLSearchParams(window.location.search);
-                const errCode = errUrlPrams.get('error')
-                console.error(errCode);
+                const errUrlParams = new URLSearchParams(window.location.search);
+                const errCode = errUrlParams.get('error')
+                const errDesc = errUrlParams.get('error_description')
+                console.error(errCode + " : " + errDesc);
             }
         };
 
@@ -41,13 +61,6 @@ const Callback = () => {
         if (state == sessionStorage.getItem('loginState')) {
             fetchAccessToken(code);
         }
-
-        // c.f. ユーザー情報の取得：https://developers.line.biz/ja/docs/line-login/verify-id-token/
-        // const userInfoResponse = await axios.get('https://api.line.me/v2/profile', {
-        //     headers: {
-        //         'Authorization': `Bearer ${accessToken}`
-        //     }
-        // });
     }, []);
 
     return (
