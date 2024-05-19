f0cb741f7bc89d7d06c738aadddc798906e61c86
haroot06 <harappaharu0614@gmail.com>
2024-05-19 13:50:06 +0900

    Update status.py (絵文字のステータス更新)

--- status.py
+++ status.py
@@ -10,43 +10,42 @@ APP_ID = 4
 API_TOKEN = 'P1Gqws7UJpRvj8MTeenezpVAm5Ce7yrW9AeyYWHI'
 #KINTONE_URL = f'https://SUBDOMAIN.cybozu.com/k//'
 
+# キントーンのレコードを更新する関数
+def update_kintone_status(user_id, status):
+    url = f'https://SUBDOMAIN.cybozu.com/k/v1/'
+    headers = {
+        'X-Cybozu-API-Token': API_TOKEN,
+        'Content-Type': 'application/json'
+    }
+    now_utc = datetime.now(pytz.utc)
+    jst = pytz.timezone('Asia/Tokyo')
+    now_jst = now_utc.astimezone(jst)
+    payload = {
+        'app': APP_ID,
+        'updateKey': {
+            'field': 'user_id',
+            'value': user_id
+        },
+        'record': {
+            'status': {
+                'value': status
+            },
+            'timestamp': {
+                'value': now_jst.strftime('%Y-%m-%dT%H:%M:%S%z')
+            }
+        }
+    }
+    response = requests.put(url, headers=headers, json=payload)
+    if response.status_code == 200:
+        return response.json(),timestamp
+    else:
+        return None, None
+            
 @app.route('()/status', methods=['PUT'])
 def update_status():
     data = request.json
     user_id = data.get('user_id')
     status = data.get('status')
-
-    # キントーンのレコードを更新する関数
-    def update_kintone_status(user_id, status):
-        url = f'https://SUBDOMAIN.cybozu.com/k/v1/'
-        headers = {
-            'X-Cybozu-API-Token': API_TOKEN,
-            'Content-Type': 'application/json'
-        }
-        now_utc = datetime.now(pytz.utc)
-        jst = pytz.timezone('Asia/Tokyo')
-        now_jst = now_utc.astimezone(jst)
-
-        payload = {
-            'app': APP_ID,
-            'updateKey': {
-                'field': 'user_id',
-                'value': user_id
-            },
-            'record': {
-                'status': {
-                    'value': status
-                },
-                'timestamp': {
-                    'value': now_jst.strftime('%Y-%m-%dT%H:%M:%S%z')
-                }
-            }
-        }
-        response = requests.put(url, headers=headers, json=payload)
-        if response.status_code == 200:
-            return response.json(),timestamp
-        else:
-            return None, None
     
     response_json, timestamp = update_kintone_status(user_id, status)
     
